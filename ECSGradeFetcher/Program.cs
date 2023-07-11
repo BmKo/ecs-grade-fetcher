4d7fb52a6355f05e84af4262b3f06a9445a29073
BmKo <60381441+BmKo@users.noreply.github.com>
2023-07-11 20:47:55 +1200

    initial commit

---
+++ ECSGradeFetcher/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
